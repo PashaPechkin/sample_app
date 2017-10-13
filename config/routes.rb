05b424b43bd8df78c819466cd5eb95da133ad493
PashaPechkin <p.gurylev93@yandex.ru>
2017-10-14 01:16:07 +0300

    Mаршрутизатор

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+#Маршрутизатор
+#Связывает действие(/действие) в КонтроленНейм(контроллер_нейм) с ВЬЮ (View)
+#Создает страницу по адресу
+#В НАШЕМ  СЛУЧАЕ
+#Вызывает ДЕЙСТВИЕ /home в КОНТРОЛЛЕРЕ StaticPagesController(static_pages) с ВЬЮ home(static_pages)
+
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
