e06341c8e414d821beb8318c800361d51fd679dc
PashaPechkin <p.gurylev93@yandex.ru>
2017-10-14 02:07:29 +0300

    Второй тест

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,26 @@
 #   если(it) "ЧТОТО" выполнить
 #    зайти 'во вью'
 #    найди(expect)(на странице(page)).это(to) иметь_слова(have_content)('мой хуй')
+#   конец
+#   Тоже самое для вью help
 #концы
 
 require 'rails_helper'
 
 describe "Static Pages" do
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
 end
